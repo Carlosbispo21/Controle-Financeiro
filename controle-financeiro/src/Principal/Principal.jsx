eeb8ca030ee272b989378137c3dd966c0dc7427e
Carlos <carlosbispo092@gmail.com>
2025-04-11 14:33:48 -0300

    primeira parte da logica

--- controle-financeiro/src/Principal/Principal.jsx
+++ controle-financeiro/src/Principal/Principal.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Principal = () => {
+  const [descricao, setDescicao] = useState("");
+  const [valor, setValor] = useState("");
+  const [tipo, setTipo] = useState("entrada");
+  const [transacoes, setTransacoes] = useState([]);
+
+  const adicionarTrancasoes = () => {
+    if (!descricao || !valor) return;
+
+    const valorNumerico = parseFloat(valor);
+
+    const novaTransacao = {
+      id: Date.now(),
+      descricao,
+      valor: valorNumerico,
+      valor,
+    };
+
+    setTransacoes([...transacoes, novaTransacao]);
+
+    setDescicao("");
+    setValor("");
+    setTipo("entrada");
+  };
   return (
     <div className="container">
       <h1>Controle Financeiro</h1>
@@ -19,21 +42,43 @@ const Principal = () => {
         </div>
 
         <div className="desc">
-          <input type="text" placeholder="Descrição" />
-          <input type="text" inputMode="numeric" placeholder="Valor" />
+          <input
+            type="text"
+            placeholder="Descrição"
+            value={descricao}
+            onChange={(e) => setDescicao(e.target.value)}
+          />
+
+          <input
+            type="text"
+            inputMode="numeric"
+            placeholder="Valor"
+            value={valor}
+            onChange={(e) => setValor(e.target.value)}
+          />
         </div>
 
         <div className="ent-sai">
           <label>
-            <input type="radio" value="entrada" />
+            <input
+              type="radio"
+              value="entrada"
+              checked={tipo === "entrada"}
+              onChange={(e) => setTipo(e.target.value)}
+            />
             Entrada
           </label>
           <label>
-            <input type="radio" value="saida" />
+            <input
+              type="radio"
+              value="saida"
+              checked={tipo === "saida"}
+              onChange={(e) => setTipo(e.target.value)}
+            />
             Saída
           </label>
         </div>
-        <button>Adicionar</button>
+        <button onClick={adicionarTrancasoes}>Adicionar</button>
 
         <table>
           <thead>
